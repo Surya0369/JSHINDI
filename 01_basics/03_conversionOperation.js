d3b0c738f234a36c1808f30b5961d0b04aa90069
Surya <32494558+Surya0369@users.noreply.github.com>
2025-08-08 15:56:41 +0000

    Conversion of dataType

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,36 @@
+// let score = 33
+// let score = "33"
+// let score = "33aaa"
+// let score = null
+// let score = undefined
+// let score = true
+// let score = "Surya"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33dsdd" => NaN
+// true => 1;
+// false => 0
+
+let IsLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(IsLoggedIn)
+
+// console.log(booleanIsLoggedIn)
+
+// 1 => true
+// 0 => false 
+// "" => false
+// "surya" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
