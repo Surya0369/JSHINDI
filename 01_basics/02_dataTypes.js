eff3947c54a074c3abdaa8aba5dc9b858b38caa6
Surya <32494558+Surya0369@users.noreply.github.com>
2025-08-08 03:30:28 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treate all JS code as newer version
+
+// alert("Hello") we are using nodejs, not browser
+
+console.log(3 
+    
+    + 3); // code readability should be high
+    
+console.log("Surya")
+
+let name = "surya"
+let age = 24
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 54
+// bigint
+// string => ""
+// boolean => true/false
+// null = stand alone value
+// undefined => 
+// symbol => unique
+
+// objects
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
